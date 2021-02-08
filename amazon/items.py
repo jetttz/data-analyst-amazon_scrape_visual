baccbced8213a74c87b0863ddbdae58c07acc015
Jet <jet@Jets-MacBook-Pro.local>
2021-02-08 16:15:55 -0600

    s

--- amazon/items.py
+++ amazon/items.py
@@ -15,12 +15,11 @@ class AmazonItem(scrapy.Item):
         elif 'Brand: ' in brand:
             return brand.replace('Brand: ',"").strip()
     def activeseller_clean(seller):
-        if 'New' in seller:
-            try:
-                return re.search('New \((.*)\) from ', seller).group(1)
-            except:
-                return "1"
-        
+        try:
+            return re.search('\((.*)\) from ', seller).group(1)
+        except:
+            return "1"
+    
         return "1"
 
     def tr(value):
@@ -35,6 +34,8 @@ class AmazonItem(scrapy.Item):
         return value.replace(':',"").strip()                 
     def getweight(value):
         return value.split(" inches; ")[1]
+    def reaplacedimension(value):
+        return value.replace('Package Dimensions:',"").strip()
 
 
     url = scrapy.Field(input_processor=MapCompose(addurl),output_processor = TakeFirst())
@@ -52,14 +53,6 @@ class AmazonItem(scrapy.Item):
     Dimensions = scrapy.Field(output_processor = TakeFirst())
     firstdate = scrapy.Field(input_processor=MapCompose(remove_tags,replacenewspace),output_processor = TakeFirst())
 
-    
-
-#test = '<tr>\n<th class="a-color-secondary a-size-base prodDetSectionEntry">\nBest Sellers Rank\n</th>\n<td>\n<span>\n\n<span>#2,455 in Home &amp; Kitchen (<a href="/gp/bestsellers/home-garden/ref=pd_zg_ts_home-garden">See Top 100 in Home &amp; Kitchen</a>)</span>\n<br>\n\n<span>#12 in <a href="/gp/bestsellers/home-garden/3733671/ref=pd_zg_hrsr_home-garden">Home Office Desks</a></span>\n<br>\n\n</span>\n</td>\n</tr>'
-#test = '<li><span class="a-list-item">\n\n\n\n<span class="a-text-bold">\nBest Sellers Rank:\n</span>\n#8 in <a href="/gp/bestsellers/pc/6795233011/ref=pd_zg_hrsr_pc">Lightning Cables</a>\n<ul class="a-unordered-list a-nostyle a-vertical zg_hrsr">\n\n</ul>\n\n\n\n</span></li>'
-#t='<tr>\n<th class="a-color-secondary a-size-base prodDetSectionEntry">\nProduct Dimensions\n</th>\n\n<td class="a-size-base">\n67.1 x 47.3 x 29.5 inches\n</td>\n</tr>'
-#print('Package Dimensions'  in t)
-#print('Best Sellers' in test)
-#t='asfasdfd'
-#print(remove_tags(t))
-#test = '<tr>\n<th class="a-color-secondary a-size-base prodDetSectionEntry">\nProduct Dimensions\n</th>\n\n<td class="a-size-base">\n39.37 x 23.62 x 29.53 inches\n</td>\n</tr>'
-#print('Package Dimensions'  in AmazonItem.replacenewspace(test) or 'Product Dimensions'  in AmazonItem.replacenewspace(test) or 'inches' in AmazonItem.replacenewspace(test))
+test = ''
+#print(AmazonItem.replacenewspace(remove_tags(test)))
+print(AmazonItem.activeseller_clean(AmazonItem.replacenewspace(remove_tags(test))))
--- amazon/spiders/searchlist.py
+++ amazon/spiders/searchlist.py
@@ -13,7 +13,8 @@ from ..items import AmazonItem
 
 class searchquery(scrapy.Spider):
 	name = 'listing'
-	start_urls =['https://www.amazon.com/s?k=table']
+	count = 1
+	start_urls =['https://www.amazon.com/s?k=pen']
 	def replacenewspace(self,value):
 		return value.replace('\n',"").strip()
 	def reaplacedimension(self,value):
@@ -29,59 +30,57 @@ class searchquery(scrapy.Spider):
 	
 	def parse(self,response):
 		for products in response.css('div.s-result-item.s-asin'):
-			l = ItemLoader(item=AmazonItem(),selector=products)
-			l.add_css('url','a.a-link-normal.a-text-normal::attr(href)')
-			l.add_css('img','img::attr(src)')
+			a = ItemLoader(item=AmazonItem(),selector=products)
+			a.add_css('url','a.a-link-normal.a-text-normal::attr(href)')
+			a.add_css('img','img::attr(src)')
 			if len(response.css('span.a-size-base-plus.a-color-base.a-text-normal'))  > 1 :
-				l.add_css('title','span.a-size-base-plus.a-color-base.a-text-normal')
+				a.add_css('title','span.a-size-base-plus.a-color-base.a-text-normal')
 			else:
-				l.add_css('title','span.a-size-medium.a-color-base.a-text-normal')
-			l.add_css('rating','span.a-icon-alt')
-			l.add_css('reviews','span.a-size-base')
-			l.add_css('price','span.a-offscreen')
-			l.add_css('ASIN','div.s-result-item.s-asin::attr(data-asin)')
+				a.add_css('title','span.a-size-medium.a-color-base.a-text-normal')
+			a.add_css('rating','span.a-icon-alt')
+			a.add_css('reviews','span.a-size-base')
+			a.add_css('price','span.a-offscreen')
+			a.add_css('ASIN','div.s-result-item.s-asin::attr(data-asin)')
+			yield scrapy.Request('https://www.amazon.com'+ products.css('a.a-link-normal.a-text-normal::attr(href)').get(), callback=self.parse_2, meta={'loader': a},)
 			
-			yield scrapy.Request('https://www.amazon.com'+ products.css('a.a-link-normal.a-text-normal::attr(href)').get(), callback=self.parse_2, meta={'loader': l},)
+			
 			
 	def parse_2(self,response):
-		loader = response.meta['loader']
-		l = ItemLoader(item=AmazonItem(),selector=response, parent=loader)
+		l = ItemLoader(item=AmazonItem(),selector=response, parent=response.meta['loader'])
 		l.add_css('brand','a[id="bylineInfo"]')
-		l.add_css('buybox','a[id="sellerProfileTriggerId"]')
+		if len(response.css('span.tabular-buybox-text')) >0:
+			l.add_css('buybox','span.tabular-buybox-text')
+		else:
+			l.add_css('buybox','a[id="sellerProfileTriggerId"]')
+			
 		l.add_xpath('activeseller','//*[@id="olp_feature_div"]/div[2]')
 		
 		if len(response.css('div.a-row.a-spacing-base').css('tr')) > 0:
-			for info in response.css('div.a-row.a-spacing-base').css('tr'):
-				l = ItemLoader(item=AmazonItem(),selector=info, parent=loader)
-				if 'Item Weight' in self.replacenewspace(info.get()):
-					weight = self.replacenewspace(remove_tags(info.css('td::text').get()))
-					l.add_value('weight',weight)
-				elif 'Package Dimensions'  in self.replacenewspace(info.get()) or 'Product Dimensions'  in self.replacenewspace(info.get()) or 'inches' in self.replacenewspace(info.get()) :
 
-					l.add_value('Dimensions',self.reaplacedimension(self.replacenewspace(remove_tags(info.css('td::text').get()))))
-				elif 'Date First' in self.replacenewspace(info.get()):
-					l.add_css('firstdate','td')
-				elif 'Best Sellers' in self.replacenewspace(info.get()):
-					l.add_value('bestseller',self.replacenewspace(remove_tags(info.get())))
+			for infoss in response.css('div.a-row.a-spacing-base').css('tr'):
+				c = ItemLoader(item=AmazonItem(),selector=infoss, parent=response.meta['loader'])
+
+				if 'Item Weight' in self.replacenewspace(infoss.get()):
+					c.add_value('weight',self.replacenewspace(remove_tags(infoss.css('td::text').get())))
+				elif 'inches' in self.replacenewspace(infoss.get()) :
+					c.add_value('Dimensions',self.replacenewspace(remove_tags(infoss.css('td::text').get())))
+				elif 'Date First' in self.replacenewspace(infoss.get()):
+					c.add_css('firstdate','td')
+				elif 'Best Sellers' in self.replacenewspace(infoss.get()):
+					c.add_value('bestseller',self.replacenewspace(remove_tags(infoss.get())))
+			yield c.load_item()
 
 		elif len(response.css('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list').css('li')) > 0:
 			for info in response.css('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list').css('li'):
-				l = ItemLoader(item=AmazonItem(),selector=info, parent=loader)
+
+				d = ItemLoader(item=AmazonItem(),selector=info, parent=response.meta['loader'])
 				if 'Ounces' in self.replacenewspace(info.get()):
-					weight =self.replacenewspace(self.getweight(remove_tags(info.css('span').get()))[1])
-					Dimensions = self.reaplacedimension2(self.reaplacedimension(self.replacenewspace(self.getweight(remove_tags(info.css('span').get()))[0])))
-					
-					l.add_value('weight',weight)
-					l.add_value('Dimensions',Dimensions)
+					d.add_value('weight',self.replacenewspace(self.getweight(remove_tags(info.css('span').get()))[1]))
+					d.add_value('Dimensions',self.reaplacedimension2(self.reaplacedimension(self.replacenewspace(self.getweight(remove_tags(info.css('span').get()))[0]))))
 				elif 'Date First' in self.replacenewspace(info.get()):
-					l.add_value('firstdate',self.reaplaceddatefirst(self.replacenewspace(remove_tags(info.get()))))
+					d.add_value('firstdate',self.reaplaceddatefirst(self.replacenewspace(remove_tags(info.get()))))
 
 				elif 'Sellers' in self.replacenewspace(info.get()):
-					l.add_value('bestseller',remove_tags(info.get()))
-
-
-
-
-
-		yield l.load_item()
+					d.add_value('bestseller',remove_tags(info.get()))
+			yield d.load_item()
 
