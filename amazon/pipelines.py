6c00750bd9cd0ba0c03976be4850f9bbaadeac94
Jet <jet@Jets-Mac-mini.local>
2021-02-26 14:18:10 -0600

    save to database

--- amazon/pipelines.py
+++ amazon/pipelines.py
@@ -1,18 +1,13 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy.exporters import CsvItemExporter
-import sys,psycopg2
+import sys,psycopg2,os
+sys.path.append(os.getcwd()+'/spiders')
+import searchlist
 
 class AmazonPipeline:
-	'''
+	
 	def open_spider(self,spider):
-		self.file = open("listing.csv", 'wb')
+		self.file = open(searchlist.searchquery.searchitem.replace(' ','_')+".csv", 'wb')
 		self.exporter = CsvItemExporter(self.file)
 		self.exporter.start_exporting()
 		self.hostname = '192.168.1.185'
@@ -22,27 +17,15 @@ class AmazonPipeline:
 		self.conn = psycopg2.connect(host = self.hostname,database=self.database,user=self.username, password=self.password, port = 5432)
 		self.conn.autocommit = True
 		self.cursor = self.conn.cursor()
+		self.filename = searchlist.searchquery.searchitem.replace(' ','_').strip()
+		self.cursor.execute('DROP TABLE IF EXISTS {};'.format(self.filename))
+		self.cursor.execute('CREATE TABLE {} (url TEXT,img TEXT,title TEXT,rating FLOAT,reviews FLOAT,price FLOAT,brand TEXT,ASIN  TEXT,description TEXT,weight_lbs  FLOAT,buybox TEXT,bestseller TEXT,activeseller FLOAT,Dimensions TEXT,firstdate TEXT);'.format(self.filename))
 
 	def close_spider(self,spider):
 		self.cursor.close()
 		self.conn.close()
 		self.exporter.finish_exporting()
 		self.file.close()
-'''
-	
-	def open_spider(self,spider):
-		self.file = open("listing.csv", 'wb')
-		
-		self.exporter = CsvItemExporter(self.file)
-		
-		self.exporter.start_exporting()
-		
-
-	def close_spider(self,spider):
-		self.exporter.finish_exporting()
-		
-		self.file.close()
-		
 		
 	def process_item(self,item,spider):
 		item.setdefault('Dimensions','n/a')
@@ -60,11 +43,8 @@ class AmazonPipeline:
 		item.setdefault('activeseller','0')
 		item.setdefault('firstdate','n/a')
 		item.setdefault('description','n/a')
-		#url = "INSERT INTO iphone_charger(url) VALUES ('%s')" % item['url']
-		#self.cursor.execute("insert into iphone_charger(description) values(%s)",(item['description'],))
-		#self.cursor.execute(url)
 		self.exporter.export_item(item)
-		#self.cursor.execute("insert into iphone_charger(url,img,title,rating,reviews,price,brand,asin,description,weight_lbs,buybox,bestseller,activeseller,dimensions,firstdate) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(item['url'],item['img'],item['title'],item['rating'],item['reviews'],item['price'],item['brand'],item['ASIN'],item['description'],item['weight_lbs'],item['buybox'],item['bestseller'],item['activeseller'],item['Dimensions'],item['firstdate']))
+		self.cursor.execute("insert into "+ self.filename + " (url,img,title,rating,reviews,price,brand,asin,description,weight_lbs,buybox,bestseller,activeseller,dimensions,firstdate) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(item['url'],item['img'],item['title'],item['rating'],item['reviews'],item['price'],item['brand'],item['ASIN'],item['description'],item['weight_lbs'],item['buybox'],item['bestseller'],item['activeseller'],item['Dimensions'],item['firstdate']))
 		return item
 
 
--- amazon/spiders/searchlist.py
+++ amazon/spiders/searchlist.py
@@ -5,14 +5,14 @@ from scrapy.loader import ItemLoader
 #from scrapy.crawler import CrawlerRunner
 #from scrapy.utils.log import configure_logging
 from w3lib.html import remove_tags
-#import sys
-#sys.path.append('../')
-from ..items import AmazonItem
+import sys
+sys.path.append('../')
+from items import AmazonItem
 
 class searchquery(scrapy.Spider):
 	name = 'listing'
 
-	searchitem = 'iphone charger'
+	searchitem = 'mouse trap'
 	
 	start_urls =['https://www.amazon.com/s?k='+searchitem.replace(' ','+').strip()]
 	
@@ -31,10 +31,10 @@ class searchquery(scrapy.Spider):
 			a.add_css('ASIN','div.s-result-item.s-asin::attr(data-asin)')
 			yield scrapy.Request('https://www.amazon.com'+ products.css('a.a-link-normal.a-text-normal::attr(href)').get(), callback=self.parse_2, meta={'loader': a},)
 
-		next_page = response.css("li.a-last").css("a::attr(href)").get()
+		#next_page = response.css("li.a-last").css("a::attr(href)").get()
 
-		if next_page:
-			yield scrapy.Request('https://www.amazon.com'+next_page, callback=self.parse)
+		#if next_page:
+			#yield scrapy.Request('https://www.amazon.com'+next_page, callback=self.parse)
 			
 			
 			
